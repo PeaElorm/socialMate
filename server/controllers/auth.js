89a1120edab1dec80633fbcddf5d8f5d7ce97347
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-07-26 16:20:23 +0000

    authorization & authentification backend starter

---
+++ server/controllers/auth.js
@@ -0,0 +1,41 @@
+// Allow for password encryption
+import bcrypt from "bcrypt";
+//give user a webtoken for authorization
+import { Jwt } from "jsonwebtoken";
+import user from "../models/User.js";
+
+// Register User
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new user({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.randon() * 1000),
+      impressions: Math.floor(Math.randon() * 1000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
